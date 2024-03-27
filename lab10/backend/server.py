63a7aff9add75b61c9b1e404582db7d4dcda74ba
jlhuelar <huelarjl18@gmail.com>
2024-03-27 13:40:54 -0600

    server.py

--- lab10/backend/server.py
+++ lab10/backend/server.py
@@ -6,24 +6,24 @@ app = Flask( __name__ )
 
 def load_products () :
     with open ('products.json', 'r') as f :
-        return json . load ( f ) [ ' products ']
+        return json.load(f)['products']
     
 
 @app.route ('/products', methods =['GET'])
-@app.route ('/products/ < int : product_id > ' , methods =[ ' GET '])
+@app.route ('/products/<int:product_id>', methods=['GET'])
 def get_products(product_id = None) :
-    products = load_products ()
+    products = load_products()
     if product_id is None :
         # Return all products wrapped in an object with a ' products ' key
         return jsonify({"products": products})
     else :
-        product = next((p for p in products if p ['id'] == product_id) ,None )
+        product = next((p for p in products if p ['id'] == product_id), None )
         
         # If a specific product is requested ,
         # wrap it in an object with a ' products ' key
         # Note : You might want to change this
         # if you want to return a single product not wrapped in a list
-        return jsonify ( product ) if product else ('', 404)
+        return jsonify(product) if product else ('', 404)
     
 
 @app.route('/products/add', methods=['POST'])
