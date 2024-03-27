8fb25b3fe83baa85944b24a7fdbfbad316019362
jlhuelar <huelarjl18@gmail.com>
2024-03-27 13:54:37 -0600

    commit

--- lab10/backend/server.py
+++ lab10/backend/server.py
@@ -4,20 +4,20 @@ import os
 
 app = Flask( __name__ )
 
-def load_products() :
+def load_products () :
     with open ('products.json', 'r') as f :
-        return json.load(f)['products']
+        return json . load ( f ) [ ' products ']
     
 
-@app.route('/products', methods =['GET'])
-@app.route('/products/<int:product_id>', methods=['GET'])
+@app.route ('/products', methods =['GET'])
+@app.route ('/products/<int:product_id>', methods=['GET'])
 def get_products(product_id = None) :
-    products = load_products ()
+    products = load_products()
     if product_id is None :
         # Return all products wrapped in an object with a ' products ' key
         return jsonify({"products": products})
     else :
-        product = next((p for p in products if p ['id'] == product_id) ,None )
+        product = next((p for p in products if p ['id'] == product_id), None )
         
         # If a specific product is requested ,
         # wrap it in an object with a ' products ' key
