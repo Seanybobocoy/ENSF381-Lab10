cb8278a65efb4033f54c3dc374607b6b0b90202b
jlhuelar <huelarjl18@gmail.com>
2024-03-27 19:41:56 -0600

    indenting

--- lab10/backend/server.py
+++ lab10/backend/server.py
@@ -11,16 +11,15 @@ def load_products () :
         return json.load(f)['products']
     
 
-@app.route ('/products', methods =['GET'])
-@app.route ('/products/<int:product_id>', methods=['GET'])
-def get_products(product_id = None) :
+@app.route('/products', methods=['GET'])
+@app.route('/products/<int:product_id>', methods=['GET'])
+def get_products(product_id = None):
     products = load_products()
-    if product_id is None :
+    if product_id is None:
         # Return all products wrapped in an object with a ' products ' key
         return jsonify({"products": products})
-    else :
-        product = next((p for p in products if p ['id'] == product_id), None )
-        
+    else:
+        product = next((p for p in products if p['id'] == product_id), None)
         # If a specific product is requested ,
         # wrap it in an object with a ' products ' key
         # Note : You might want to change this
