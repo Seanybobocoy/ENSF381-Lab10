bdcd79cc57f47ef6ff152b8212564b8803627a7a
jlhuelar <huelarjl18@gmail.com>
2024-03-27 20:07:53 -0600

    more indenting

--- lab10/backend/server.py
+++ lab10/backend/server.py
@@ -6,24 +6,19 @@ import os
 app = Flask( __name__ )
 CORS(app)
 
-def load_products () :
-    with open ('products.json', 'r') as f :
+def load_products() :
+    with open('products.json', 'r') as f:
         return json.load(f)['products']
     
 
 @app.route('/products', methods=['GET'])
 @app.route('/products/<int:product_id>', methods=['GET'])
-def get_products(product_id = None):
+def get_products(product_id=None):
     products = load_products()
     if product_id is None:
-        # Return all products wrapped in an object with a ' products ' key
         return jsonify({"products": products})
     else:
         product = next((p for p in products if p['id'] == product_id), None)
-        # If a specific product is requested ,
-        # wrap it in an object with a ' products ' key
-        # Note : You might want to change this
-        # if you want to return a single product not wrapped in a list
         return jsonify(product) if product else ('', 404)
     
 
